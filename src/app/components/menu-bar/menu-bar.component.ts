be66033fa83f237a58c01a97521bd39907a3f99f
Adriano Lincoln <sit-bull@hotmail.com>
2025-03-23 23:04:08 -0300

    components createds with styles

---
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-bar',
+  imports: [],
+  templateUrl: './menu-bar.component.html',
+  styleUrl: './menu-bar.component.css'
+})
+export class MenuBarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    // Initialization logic here
+  }
+
+  // Add any additional methods or properties here
+
+}
---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  imports: [],
+  templateUrl: './small-card.component.html',
+  styleUrl: './small-card.component.css'
+})
+export class SmallCardComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+    // Initialization logic here
+  }
+
+}
